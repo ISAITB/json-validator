b7ac99044feabdbf13d9a5553615acbb86ff4c0b
Costas Simatos <costas.simatos@sigmacubed.eu>
2021-07-28 15:17:38 +0200

    Commons WAR refactoring

--- jsonvalidator-war/src/main/java/eu/europa/ec/itb/json/webhook/StatisticReportingAspect.java
+++ jsonvalidator-war/src/main/java/eu/europa/ec/itb/json/webhook/StatisticReportingAspect.java
@@ -9,6 +9,8 @@ import javax.xml.ws.handler.MessageContext;
 import com.gitb.tr.TAR;
 import com.gitb.tr.TestResultType;
 
+import eu.europa.ec.itb.validation.commons.war.webhook.StatisticReporting;
+import eu.europa.ec.itb.validation.commons.war.webhook.UsageData;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -21,8 +23,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
-import eu.europa.ec.itb.commons.war.webhook.StatisticReporting;
-import eu.europa.ec.itb.commons.war.webhook.UsageData;
 import eu.europa.ec.itb.json.ApplicationConfig;
 import eu.europa.ec.itb.json.gitb.ValidationServiceImpl;
 import eu.europa.ec.itb.json.validation.JSONValidator;
