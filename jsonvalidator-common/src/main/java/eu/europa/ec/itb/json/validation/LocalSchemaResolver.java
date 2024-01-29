9db63768541d4b67d380d9c1d24ef81517279589
simatosc <costas.simatos@sigmacubed.eu>
2024-01-29 10:24:35 +0100

    Upgrade to json-schema-validator v1.3.0

--- jsonvalidator-common/src/main/java/eu/europa/ec/itb/json/validation/LocalSchemaResolver.java
+++ jsonvalidator-common/src/main/java/eu/europa/ec/itb/json/validation/LocalSchemaResolver.java
@@ -1,25 +1,24 @@
 package eu.europa.ec.itb.json.validation;
 
-import com.networknt.schema.uri.URIFetcher;
-import com.networknt.schema.uri.URLFetcher;
+import com.networknt.schema.AbsoluteIri;
+import com.networknt.schema.resource.InputStreamSource;
+import com.networknt.schema.resource.SchemaLoader;
+import com.networknt.schema.resource.UriSchemaLoader;
 import eu.europa.ec.itb.json.DomainConfig;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
 import java.nio.file.Files;
 
 /**
  * Custom schema resolver that loads schemas defined as local references.
  */
-public class LocalSchemaResolver implements URIFetcher {
+public class LocalSchemaResolver implements SchemaLoader {
 
     private static final Logger LOG = LoggerFactory.getLogger(LocalSchemaResolver.class);
 
-    private final URLFetcher urlFetcher = new URLFetcher();
+    private final UriSchemaLoader uriSchemaLoader = new UriSchemaLoader();
     private final DomainConfig domain;
     private final LocalSchemaCache localSchemaCache;
 
@@ -37,20 +36,19 @@ public class LocalSchemaResolver implements URIFetcher {
     /**
      * Resolve a schema based on the provided ID.
      *
-     * @param id the identifier of the schema to return.
+     * @param absoluteIri the identifier of the schema to return.
      * @return The resolved schema or null if none could be resolved from the configured shared schema references.
      */
     @Override
-    public InputStream fetch(URI id) throws IOException {
-        String idToCheck = StringUtils.appendIfMissing(id.toString(), "#");
+    public InputStreamSource getSchema(AbsoluteIri absoluteIri) {
+        String idToCheck = StringUtils.appendIfMissing(absoluteIri.toString(), "#");
         var schema = localSchemaCache.getSchemaForId(domain, idToCheck);
         if (schema.isEmpty()) {
-            LOG.debug("Schema with URI {} not found locally. Looking up remotely.", id);
-            return urlFetcher.fetch(id);
+            LOG.debug("Schema with URI {} not found locally. Looking up remotely.", absoluteIri);
+            return uriSchemaLoader.getSchema(absoluteIri);
         } else {
-            LOG.debug("Schema with URI {} found locally.", id);
-            return Files.newInputStream(schema.get());
+            LOG.debug("Schema with URI {} found locally.", absoluteIri);
+            return () -> Files.newInputStream(schema.get());
         }
     }
-
 }
